a35f87872667d467117d449d7c9f0090e56c35f8
Luis Jimenez <minicaleb16@hotmail.com>
2022-10-10 22:38:28 -0500

    clean up simulation code and switch to classes

--- simulation.py
+++ simulation.py
@@ -10,28 +10,35 @@ difs = 4
 simulation_slots = 1000000
 bandwidth = 8*10**(6)
 
-def traffic_generator(arrival_rate):
-	# Generate uniform distribution, Multipling size times two just to ensure enough samples to transmit
-	uniform_distribution = np.random.uniform(low=0, high=1, size=arrival_rate * simulation_time * 2)
-	# Convert to exponential distribution
-	exponential_distribution = exponential_distribution = -(1 / arrival_rate) * np.log(1 - uniform_distribution)
-	# Convert to interpacket slot times
-	interpacket_time_slot = np.ceil(exponential_distribution / slot_duration)
-	# Find the slot arrival time of a packet
-	arrival_time_slot = np.cumsum(interpacket_time_slot)
 
+def generate_uniform_distribution(sample_size):
+	return np.random.uniform(low=0, high=1, size=sample_size)
+
+def convert_to_exponential_distribution(uniform_distribution, arrival_rate):
+	exponential_distribution = -(1 / arrival_rate) * np.log(1 - uniform_distribution)
+	return exponential_distribution
+
+def convert_to_slot_time(exponential_distribution):
+	return np.ceil(exponential_distribution / slot_duration)
+
+def convert_to_arrival_slot_time(interpacket_slot):
+	return np.cumsum(interpacket_slot)
+
+def traffic_generator(arrival_rate):
+	uniform_distribution = generate_uniform_distribution(arrival_rate * simulation_time * 2)
+	exponential_distribution = convert_to_exponential_distribution(uniform_distribution, arrival_rate)
+	interpacket_time_slot = convert_to_slot_time(exponential_distribution)
+	arrival_time_slot = convert_to_arrival_slot_time(interpacket_time_slot)
 	return arrival_time_slot
 
-def transmit_packet(current_time_slot, backoff):
+def transmit_packet(backoff = np.random.randint(0, cw_base*2**extendedBackoff)):
 	current_time_slot = current_time_slot + difs + backoff + sifs + ack
 
 def simulation(arrival_slot_1, arrival_slot_2):
 	slot_index_1 = 0
 	slot_index_2 = 0
 	backoff_1 = 0
-	backoff_2 = 0
-	arrival_slot_1
-	arrival_slot_2
+	backoff_2 = 0:
 	extendedBackoff = 0
 	collisionCounter = 0
 
@@ -39,63 +46,69 @@ def simulation(arrival_slot_1, arrival_slot_2):
 	current_time_slot = arrival_slot_1[0] if arrival_slot_1[0] <= arrival_slot_2[0] else arrival_slot_2[0]
 
 	# Start simulation until simulation time exceeded
-	while simulation_slots > current_time_slot:
-		if backoff_1 == 0 or extendedBackoff != 0:
-			backoff_1 = np.random.randint(0, cw_base*2**extendedBackoff)
-		if backoff_2 == 0 or extendedBackoff != 0:
-			backoff_2 = np.random.randint(0, cw_base*2**extendedBackoff)
-		
-		# Check if more than two frames are to compete for the same medium
-		if arrival_slot_1[slot_index_1] <= current_time_slot and arrival_slot_2[slot_index_2] <= current_time_slot:
-			# Check if for possible packet collision or detection of medium usage
+	while simulation_slots > current_time_slot:s
+
+		# Check if for possible packet collision or detection of medium usage. Stay in loop until collisions have been cleared
+		while arrival_slot_1[slot_index_1] <= current_time_slot and arrival_slot_2[slot_index_2] <= current_time_slot:
+			if backoff_1 == 0:
+				backoff_1 = np.random.randint(0, cw_base*2**extendedBackoff)
+			if backoff_2 == 0:
+				backoff_2 = np.random.randint(0, cw_base*2**extendedBackoff)
+
 			if backoff_1 == backoff_2:
 				current_time_slot = current_time_slot + difs + backoff_1 + sifs + ack
-				extendedBackoff += 1
+				extendedBackoff += 1;
 				collisionCounter = collisionCounter + 1
+
 			elif backoff_1 < backoff_2:
-				transmit_packet(current_time_slot, backoff_1)
-				slot_index_1 += 1
-				# Reset transmission values
+				transmit_packet(backoff_1)
 				backoff_1 = 0
-				extendedBackoff = 0
-				# Adjust backoff of competing frame
-				backoff_2 = backoff_2 - backoff_1
+				extendedBackoff = 0;
+				slot_index_1 += 1
+				
+				backoff_2 = backoff_2 - backoff_1;
+
 			else:
-				transmit_packet(current_time_slot, backoff_2)
-				slot_index_2 += 1
-				# Reset transmission values
+				transmit_packet(backoff_2)
 				backoff_2 = 0
-				extendedBackoff = 0
-				# Adjust backoff of competing frame
-				backoff_1 = backoff_1 - backoff_2
+				extendedBackoff = 0;
+				slot_index_2 += 1
+
+				backoff_1 = backoff_1 - backoff_2;
+
+		# Reset extended backoff
+		extendedBackoff = 0;
+
+		# Start transmittion of a packet
 		elif arrival_slot_1[slot_index_1] <= current_time_slot:
-			transmit_packet(current_time_slot, backoff_1)
-			backoff_1 = 0
+			transmit_packet()
 			slot_index_1 += 1
+
 		elif arrival_slot_2[slot_index_2] <= current_time_slot:
-			transmit_packet(current_time_slot, backoff_2)
-			backoff_2 = 0
+			transmit_packet()
 			slot_index_2 += 1
+
 		# In idle, thus increment the time slot till something to transmit
 		else:
 			current_time_slot = current_time_slot + 1
 
+
 	# Packet count that have been sent succesfully
 	number_of_successes_1 = slot_index_1
 	number_of_successes_2 = slot_index_2
-	# Throughput
+
+	# Calculate throughput
 	throughput = number_of_successes_1 * (bandwidth / simulation_time)
+
 	# Fairness Index
 	fairness_index = number_of_successes_1 / number_of_successes_2
 
 	return collisionCounter, throughput, fairness_index
 
 def main():
-	arrival_rates = [100, 200, 300, 400, 700, 1000]
-	for arrival_rate in arrival_rates:
-		arrival_slot_1 = traffic_generator(arrival_rate)
-		arrival_slot_2 = traffic_generator(arrival_rate)
-		collisions, throughput, fi = simulation(arrival_slot_1, arrival_slot_2)
-		print("Collsions: {0}, Throughput: {1}, Fairness Index: {2}".format(collisions, throughput, fi))
+	arrival_rate = 1
+	arrival_slot_1 = traffic_generator(arrival_rate)
+	arrival_slot_2 = traffic_generator(arrival_rate)
+	collisions, throughput, fi = simulation(arrival_slot_1, arrival_slot_2)
 
 main()
